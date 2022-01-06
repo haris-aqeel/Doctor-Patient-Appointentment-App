2f06e54a4bc4ca9527c8bc24104049769cd0eb96
haris-aqeel <harisaqeel.2001@gmail.com>
2022-01-06 21:08:21 +0500

    Added

---
+++ src/components/Routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Switch, Route } from 'react-router-dom'; 
+import Home from './pages/Home';
+import AppointmentPage from './pages/AppointmentPage';
+import AppointmentForm from './pages/homePagesComponents/AppointmentForm';
+import Login from './pages/dashboard/Login';
+import SignUp from './pages/dashboard/SignUp';
+import DashboardAppointmentPage from './pages/dashboard/Appointments/DashboardAppointmentPage';
+import Patients from './pages/dashboard/patients/Patients';
+import PrivateRoute from './privateRoute/PrivateRoute';
+import NotFoundPage from './pages/NotFoundPage';
+
+const Routes = () => {
+  return (
+    <Switch>
+        <Route exact path="/" component={Home} />;
+        <Route  path="/create-appointment" component={AppointmentPage} />;
+        <Route  path="/appointment-form" component={AppointmentForm} />;
+        <PrivateRoute path="/dashboard/appointment">
+          <DashboardAppointmentPage/>
+        </PrivateRoute>
+        <PrivateRoute path="/dashboard/patients">
+          <Patients/>
+        </PrivateRoute>
+        <Route path="/login" component={Login} />;
+        <Route path="/signup" component={SignUp} />;
+
+        <Route path="*" component={NotFoundPage} />
+    </Switch>
+  );
+};
+
+export default Routes;
---
+++ src/components/pages/dashboard/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+
+import './Auth.css'
+
+
+const Dashboard = () => {
+
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const notify = (message) => toast(message);
+
+
+
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    if (email === '') {
+      notify('Username/Email is required!');
+      localStorage.setItem('status_login', 'false');
+    }else if (password === ''){
+      notify('Password is required!');
+      localStorage.setItem('status_login', 'false');
+    }else{
+      localStorage.setItem('status_login', 'true');
+      history.push('/');
+    }
+  };
+
+  return (
+    <div className="containers">
+      <div className="screen">
+        <div className="screen__content">
+          <form className="login" onSubmit={loginHandler}>
+            <div className="login__field">
+              <i className="login__icon fas fa-user" />
+              <input type="text" className="login__input" placeholder="User name / Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock" />
+              <input type="password" className="login__input" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button className="button login__submit" type="submit">
+              <span className="button__text">Log In Now</span>
+              <i className="button__icon fas fa-chevron-right" />
+            </button>
+          </form>
+        </div>
+        <div className="screen__background">
+          <span className="screen__background__shape screen__background__shape4" />
+          <span className="screen__background__shape screen__background__shape3" />
+          <span className="screen__background__shape screen__background__shape2" />
+          <span className="screen__background__shape screen__background__shape1" />
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Dashboard;
