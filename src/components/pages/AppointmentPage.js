c4bd04c95ed799cbb4b80e247e9174da8654c4a5
Areeb29 <areebnadeem29@gmail.com>
2022-01-15 15:22:10 -0800

    speech

--- src/components/pages/AppointmentPage.js
+++ src/components/pages/AppointmentPage.js
@@ -1,14 +1,21 @@
-import React from 'react';
+
 import Header from './homePagesComponents/Header';
 import AvailableAppointments from './homePagesComponents/AvailableAppointments';
+import TextSpeech from './homePagesComponents/TextSpeech';
+import React, { useState, useEffect, useContext } from 'react';
+// import Speech from 'react-speech';
 
 const AppointmentPage = () => {
-
+  useEffect(() => {
+    const msg = new window.SpeechSynthesisUtterance('This is appoinment page');
+    window.speechSynthesis.speak(msg);  
+}, [])
 
   return (
     <div id="main-home-page">
       <Header  />
       <AvailableAppointments  />
+      
     </div>
   );
 };
---
+++ src/components/pages/homePagesComponents/TextSpeech.js
@@ -0,0 +1,38 @@
+import { useSpeechSynthesis } from "react-speech-kit";
+import React, { useState, useEffect, useContext } from 'react';
+const Speech = () => {
+// const [value, setValue] = React.useState("This is the appoinment page");
+// const [speak, setSpeak] = React.useState();
+
+// const { speak } = useSpeechSynthesis();
+
+
+
+useEffect(() => {
+    const msg = new window.SpeechSynthesisUtterance('This is appoinment page');
+    window.speechSynthesis.speak(msg);  
+}, [])
+
+return (
+	<div className="speech">
+	{/* <div className="group">
+		<h2>Text To Speech Converter Using React Js</h2>
+	</div>
+	<div className="group">
+		<textarea
+		rows="2"
+		value={value}
+		onChange={(e) => setValue(e.target.value)}
+		></textarea>
+	</div>
+	<div className="group">
+		<button onClick={() => speak({ text: value })}>
+		Speech
+		</button>
+	</div> */}
+    {/* <button onClick={speak({ text: "This is the appoinment page" })}>
+		</button> */}
+	</div>
+);
+};
+export default Speech;
